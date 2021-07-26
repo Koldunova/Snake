5b048a2c739eaf27555f9c59a653698360d4b1a8
Koldunova Alexandra <40992427+Koldunova@users.noreply.github.com>
2021-07-26 12:52:02 +0300

    Add files via upload

---
+++ src/by/koldunova/GameFrame.java
@@ -0,0 +1,18 @@
+package by.koldunova;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	public GameFrame() {
+		
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+	
+}
---
+++ src/by/koldunova/SnakeGame.java
@@ -0,0 +1,12 @@
+package by.koldunova;
+
+//help https://www.youtube.com/watch?v=bI6e6qjJ8JQ
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		GameFrame frame = new GameFrame();
+		
+	}
+
+}
